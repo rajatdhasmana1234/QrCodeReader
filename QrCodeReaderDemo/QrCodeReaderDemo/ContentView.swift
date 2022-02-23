b08683f5403c8298229cf652e1ebd56f7f232656
Rajat Dhasmana <96765608+rajatdhasmana1234@users.noreply.github.com>
2022-02-23 18:12:22 +0530

    initial commit

---
+++ QrCodeReaderDemo/QrCodeReaderDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QrCodeReaderDemo
+//
+//  Created by RAJAT DHASMANA on 23/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QrCodeReaderDemo/QrCodeReaderDemo/QrCodeReaderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrCodeReaderDemoApp.swift
+//  QrCodeReaderDemo
+//
+//  Created by RAJAT DHASMANA on 23/02/22.
+//
+
+import SwiftUI
+
+@main
+struct QrCodeReaderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
